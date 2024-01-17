2f0931cc025abe0de50c54be835a7d25664a9515
Erin Clark <erincodepirate@gmail.com>
2024-01-16 19:10:46 -0600

    Fix linting errors

--- todo_api/models.py
+++ todo_api/models.py
@@ -7,6 +7,7 @@ class Todo(models.Model):
     timestamp = models.DateTimeField(auto_now_add = True, auto_now = False, blank = True)
     updated = models.DateTimeField(auto_now = True, blank = True)
     user = models.ForeignKey(User, on_delete = models.CASCADE, blank = True, null = True)
-    
+
     def __str__(self):
         return str(self.task)
+ 
